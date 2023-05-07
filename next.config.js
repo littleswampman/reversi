3706dbd995932f99428b4c8782e9a6bedfdc271b
littleswampman <littleswampman@gmail.com>
2023-05-07 10:06:20 +0900

    [fix] next.config.js
    next.config.jsにbasePathとassetPrefixの記述を追加した。
    
    GitHub Pagesにデプロイする #2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
+const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
 const nextConfig = {
+  basePath: '/' + repo,
+  assetPrefix: '/' + repo + '/',
   reactStrictMode: true,
   swcMinify: true,
 };
